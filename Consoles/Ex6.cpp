3d62a610dee7a08b969f3b6d50f24a6b00d4a9f3
Decompilationist <verboty@gmail.com>
2023-04-17 10:58:22 -0300

    Update Ex6.cpp

--- Consoles/Ex6.cpp
+++ Consoles/Ex6.cpp
@@ -7,5 +7,23 @@ using namespace std;
 
 int main() {
     int num;
-    bool primo;
+    bool primo = true;
+
+    cout << "Digite um numero: ";
+    cin >> num;
+
+    for (int i =2; i <= num / 2; i++) {
+        if (num % i == 0) {
+            primo = false;
+            break;
+        }
+    }
+
+    if(primo) {
+        cout << num << " e um numero primo." << endl;
+    } else {
+        cout << num << " nao e um numero primo" << endl;
+    }
+
+    return 0;
 }
