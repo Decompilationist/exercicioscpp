586b8e06cb34f29f5f6c026ae1af8694bfc1f463
Decompilationist <verboty@gmail.com>
2023-04-18 10:20:53 -0300

    Update Ex9.cpp

--- Consoles/Ex9.cpp
+++ Consoles/Ex9.cpp
@@ -1,1 +1,24 @@
 // Crie um programa que leia uma lista de nomes e ordene-os em ordem alfabética.
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int main() {
+    std::vector<std::string> nomes;
+
+    std::string nome;
+    while (std::cin >> nome) {
+        nomes.push_back(nome);
+    }
+
+    std::sort(nomes.begin(), nomes.end());
+
+    std::cout << "Nomes ordenados em ordem afabetica: \n";
+    for(const auto& n : nomes) {
+        std::cout << n << '\n';
+    }
+
+    return 0;
+    
+}
